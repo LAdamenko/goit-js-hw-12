3c9ddea5598b1af9b3d1852d34da1b11c5eff8ed
LAdamenko <148965340+LAdamenko@users.noreply.github.com>
2024-03-30 00:58:57 +0200

    axios

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,19 +1,23 @@
-export function getImges(query) {
-    const baseUrl = `https://pixabay.com/api/`;
-    const params = new URLSearchParams({
+import axios from 'axios';
+
+export async function getImges(query) {
+    const url = `https://pixabay.com/api/`;
+    const params = {
         key: '43018161-f974b102273f6260f6ed159b0',
         q: query,
         image_type: "photo",
         orientation: "horizontal",
         safesearch: "true",
-    });
-    const url = `${baseUrl}?${params}`;
-  return fetch(url).then(res => res.json()).catch(err => {
-      iziToast.error({
+    };
+    try {
+        const res = await axios.get(url, { params });
+        return res.data;
+    } catch (err) {
+        iziToast.error({
                     color: 'red',
                     message: `❌ Sorry, there was a mistake. Please try again!`,
                     position: 'topRight',
                 });
-    });
+    }
 }
 
